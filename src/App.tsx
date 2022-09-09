a36d8f577c9654558b020f5d969ed8e6ce118a73
shenxin <wb_shenxin@meituan.com>
2022-09-09 13:06:19 +0800

    init

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import { useState } from "react";
+import reactLogo from "./assets/react.svg";
+import "./App.css";
+import { Button } from "./lib";
+
+function App() {
+  return (
+    <div className="App">
+      <Button />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/lib/Button.tsx
@@ -0,0 +1,10 @@
+import React from "react";
+import './Button.scss';
+const Button: React.FC = () => {
+  return (
+    <div className="xxx">
+      <button>xxx</button>
+    </div>
+  );
+};
+export default Button;
